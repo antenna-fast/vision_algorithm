8169baaa62c8a249375df880802666ddd8e4651d
trueliuyaohua <1316587097@qq.com>
2021-01-02 22:48:17 +0800

    modify

---
+++ dist_method/test.py
@@ -0,0 +1,16 @@
+from numpy import *
+from numpy.linalg import *
+
+
+a = array([1, 0])
+b = array([1, 1])
+c = array([-1, 1])
+d = array([1, -1])
+e = array([-1, -1])
+
+# cos_ang = dot(a, b) / (norm(a)*norm(b))
+# cos_ang = dot(a, c) / (norm(a)*norm(c))
+# cos_ang = dot(a, d) / (norm(a)*norm(d))
+cos_ang = dot(a, e) / (norm(a)*norm(e))
+
+print(cos_ang)
